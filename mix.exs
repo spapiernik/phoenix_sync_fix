a990634becd7101f56774c841c96ff738a2259b1
Santiago Papiernik <spapiernik12@gmail.com>
2026-04-12 21:18:55 -0500

    feat: relax Elixir version requirement to ~> 1.17

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule PhoenixSyncFix.MixProject do
     [
       app: :phoenix_sync_fix,
       version: "0.1.0",
-      elixir: "~> 1.20-rc",
+      elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
       description: "Phoenix.Sync fork for debugging mix igniter.install",
